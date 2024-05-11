e0c2e583b242ef736c9f84ec338a87932aa9dbaa
InnocentusLime <innocentuslime.help@gmail.com>
2024-05-11 18:09:59 +0300

    Fix & improve cart API

--- webapp/api/add_to_cart.php
+++ webapp/api/add_to_cart.php
@@ -1,12 +1,21 @@
 <?php
 
-require_once "blocks.php";
+require_once "db_utils.php";
 
 if (!isset($_GET["item_id"])) {
     respond_with_error("CART_API_NO_ITEM_ID");
+    exit;
 }
 
 $item_id = intval($_GET["item_id"]);
+
+connect_to_db();
+if (!get_item_info($item_id)) {
+    respond_with_error("CART_API_NO_SUCH_ITEM");
+    exit;
+}
+disconnect_from_db();
+
 session_start_if_none();
 add_to_cart($item_id);
 respond_with_json_ok(null);
--- webapp/api/remove_from_cart.php
+++ webapp/api/remove_from_cart.php
@@ -4,6 +4,7 @@ require_once "blocks.php";
 
 if (!isset($_GET["item_id"])) {
     respond_with_error("CART_API_NO_ITEM_ID");
+    exit;
 }
 
 $item_id = intval($_GET["item_id"]);
