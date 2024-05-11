562917f387b5341d25916e608092cd62d00e0399
InnocentusLime <innocentuslime.help@gmail.com>
2024-05-11 16:26:15 +0300

    Basic link update

--- webapp/cart.php
+++ webapp/cart.php
@@ -4,12 +4,12 @@ require "blocks.php";
 
 <?php begin_common_page("main"); ?>
 
+Your items:
+
 <?php
 foreach (get_cart_items() as $item_id => $quant) {
     echo "ITEM($item_id) x $quant </br>";
 }
 ?>
-
-Hello there!
 
 <?php end_common_page(); ?>
