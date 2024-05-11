1ce1da08a22ceddbe4233f26832ee914d85fdd50
InnocentusLime <innocentuslime.help@gmail.com>
2024-05-11 09:11:00 +0300

    Basic test

---
+++ webapp/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Test</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <?php echo '<p>Hello World! PHP WOOOORKS!!!!</p>'; ?>
+        <img src ="goth.jpg"/>
+    </body>
+</html>
