e529b130c22536993d62ef3c4a0570577359abd0
InnocentusLime <innocentuslime.help@gmail.com>
2024-05-11 16:23:50 +0300

    Change incorrect item removal code

--- webapp/api/remove_from_cart.php
+++ webapp/api/remove_from_cart.php
@@ -12,5 +12,5 @@ session_start_if_none();
 if (remove_from_cart($item_id)) {
     respond_with_json_ok(null);
 } else {
-    respond_with_error("CART_API_NO_SUCH_ITEM");
+    respond_with_error("CART_API_NO_SUCH_ITEM_IN_CART");
 }
