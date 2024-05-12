f533c750acf788e75808b155319371a0564f58c8
InnocentusLime <innocentuslime.help@gmail.com>
2024-05-12 18:58:58 +0300

    add redirect

--- webapp/js/ui.js
+++ webapp/js/ui.js
@@ -10,14 +10,18 @@ async function add_to_cart(x) {
 async function logout() {
 	console.log("Logout");
 	const response = await fetch("/api/logout.php");
-	const todos = await response.json();
-	console.log(todos);
+	const data = await response.json();
+	console.log(data);
+
+	if (data.status == "ok") {
+		window.location.replace("/auth.php");
+	}
 }
 
 async function remove_from_cart(x) {
 	// do request to /api/remove_to_cart.php?item_id=x
 	console.log("Remove from cart", x);
-	const response = await fetch("/api/remove_to_cart.php?item_id=" + x);
+	const response = await fetch("/api/remove_from_cart.php?item_id=" + x);
 	const todos = await response.json();
 	console.log(todos);
 }
@@ -25,10 +29,10 @@ async function remove_from_cart(x) {
 async function do_login(event) {
 	event.preventDefault();
 	console.log("Login");
-	const new_user = 1;
+	const new_user = 0;
 	const login = document.getElementById("login").value;
 	const password = document.getElementById("passwd").value;
-	const response = await fetch("/api/auth.php?new=" + new_user + "&login=" + login + "&password=" + password);
+	const response = await fetch("/api/auth.php?new=" + new_user + "&login=" + login + "&passwd=" + password);
 	const todos = await response.json();
 	console.log(todos);
 }
@@ -36,10 +40,10 @@ async function do_login(event) {
 async function do_register(event) {
 	event.preventDefault();
 	console.log("Register");
-	const new_user = 0;
+	const new_user = 1;
 	const login = document.getElementById("login").value;
 	const password = document.getElementById("passwd").value;
-	const response = await fetch("/api/auth.php?new=" + new_user + "&login=" + login + "&password=" + password);
+	const response = await fetch("/api/auth.php?new=" + new_user + "&login=" + login + "&passwd=" + password);
 	const todos = await response.json();
 	console.log(todos);
 }
