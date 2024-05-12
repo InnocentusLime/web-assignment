a8964238d84703b0770ca021a4b2bc78c713af7b
Mikhail Sergeev <msmailgr@gmail.com>
2024-05-12 18:47:33 +0300

    Functions bodies added

--- webapp/js/ui.js
+++ webapp/js/ui.js
@@ -1,6 +1,10 @@
-function add_to_cart(x) {
+async function add_to_cart(x) {
     // do request to /api/add_to_cart.php?item_id=x
     console.log("Add to cart", x);
+	const response = await fetch("/api/add_to_cart.php?item_id=" + x);
+	const todos = await response.json();
+	console.log(todos);
+
 }
 
 async function logout() {
@@ -10,16 +14,32 @@ async function logout() {
 	console.log(todos);
 }
 
-function remove_from_cart(x) {
+async function remove_from_cart(x) {
+	// do request to /api/remove_to_cart.php?item_id=x
 	console.log("Remove from cart", x);
+	const response = await fetch("/api/remove_to_cart.php?item_id=" + x);
+	const todos = await response.json();
+	console.log(todos);
 }
 
-function do_login(event) {
+async function do_login(event) {
 	event.preventDefault();
 	console.log("Login");
+	const new_user = 1;
+	const login = document.getElementById("login").value;
+	const password = document.getElementById("passwd").value;
+	const response = await fetch("/api/auth.php?new=" + new_user + "&login=" + login + "&password=" + password);
+	const todos = await response.json();
+	console.log(todos);
 }
 
-function do_register(event) {
+async function do_register(event) {
 	event.preventDefault();
-	console.log("Register")
+	console.log("Register");
+	const new_user = 0;
+	const login = document.getElementById("login").value;
+	const password = document.getElementById("passwd").value;
+	const response = await fetch("/api/auth.php?new=" + new_user + "&login=" + login + "&password=" + password);
+	const todos = await response.json();
+	console.log(todos);
 }
