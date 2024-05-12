beb0e7b553eb631b7043967845d6f37856908c34
InnocentusLime <innocentuslime.help@gmail.com>
2024-05-12 19:36:21 +0300

    Add aboutUs page

---
+++ webapp/about_us.php
@@ -0,0 +1,40 @@
+<?php
+require "blocks.php";
+?>
+
+<?php begin_common_page("Authentication"); ?>
+
+<section class="about-us">
+    <h2>About Hattrick Store</h2>
+    <p>Hattrick Store is your go-to store for all possible trends in hats, caps, and panamas.</p>
+    <p>We aim to provide high-quality products that inspire and empower
+        our customers to express their unique style.</p>
+    <h2>Our history</h2>
+    <p>At 22:17 on May 10, 2024, one of us came up with the idea of the flux capacitor after slipping and bumping his
+        head
+        while standing on his toilet to hang a clock. The idea came to him in a vision he had after being knocked out.
+        Since coming up with the idea, he spent a lot of time and most of his family fortune trying to get it to work.
+        At 23:02, the time travel machine was ready. With such a powerful invention in our hands, the idea of how to use
+        it was laying on the surface.
+        We grabbed the Grays Sports Almanac 1950-2000 and went back to 1955 to buy some hats!
+        Brilliant idea to buy hats in the past and sell them nowadays, isn't it? And by the way, if you didn’t like our
+        product, you never bought it!
+        But that was just the beginning. After some problems with the laws of physics and matter, the portal gun was
+        invented.
+        Just one step through the portal and you're in another multiverse. I said, "That's one small step for man, one
+        giant leap for our Hattrick business."
+        There is a world where they make hats from salami (bestseller now!). And there is a multiverse where hats wear
+        people,
+        we’re not even sure what we should import from this place (how TRICKY is that, ha?).
+        We brought one hat from there, and now it teaches social psychology at Princeton.</p>
+    <p>Of course, we understand that our technologies can cause terrible harm if they fall into the wrong hands.
+        Therefore, after much discussion, we settled on the idea to make this website.</p>
+    </p>
+    <h3>Our Team</h3>
+    <ul>
+        <li>Innokentii Meleshchenko - Backend and time travel machine.</li>
+        <li>Mikhail Sergeev - Frontend and portal gun.</li>
+    </ul>
+</section>
+
+<?php end_common_page(); ?>
